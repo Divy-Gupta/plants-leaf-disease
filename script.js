ef374c9027b8754f7e44b215d11367abfcde0e76
Divy Gupta <154852793+Divy-Gupta@users.noreply.github.com>
2026-01-13 21:39:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tryBtn = document.getElementById("tryBtn");
-  const hero = document.getElementById("hero");
-  const detection = document.getElementById("detection");
 
   tryBtn.addEventListener("click", () => {
-    hero.style.opacity = "0";
-
-    setTimeout(() => {
-      hero.style.display = "none";
-      detection.style.display = "block";
-      detection.scrollIntoView({ behavior: "smooth" });
-    }, 600);
+    window.open(
+      "https://plants-leaf-disease-detection4u.streamlit.app/",
+      "_blank"
+    );
   });
 });
